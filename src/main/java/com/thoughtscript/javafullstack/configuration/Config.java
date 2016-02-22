82944b517e279e74b196157120a36400833f1ec5
facewreck <Thoughtscript@thoughtscript.io>
2016-02-22 00:30:14 -0600

    Project fixes + pom.xml

--- src/main/java/com/thoughtscript/javafullstack/configuration/Config.java
+++ src/main/java/com/thoughtscript/javafullstack/configuration/Config.java
@@ -1,8 +1,13 @@
 package com.thoughtscript.javafullstack.configuration;
 
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.view.JstlView;
@@ -12,17 +17,40 @@ import org.springframework.web.servlet.view.UrlBasedViewResolver;
 @EnableWebMvc
 @ComponentScan(basePackages = "com.thoughtscript.javabackend")
 public class Config {
-	
-	 @Bean  
-	  public UrlBasedViewResolver setupViewResolver() {  
-	      UrlBasedViewResolver resolver = new UrlBasedViewResolver();  
-	      resolver.setPrefix("/webapp/");  
-	      resolver.setSuffix(".jsp");  
-	      resolver.setViewClass(JstlView.class);  
-	      return resolver;  
-	  }  
-	  
-	  public void addResourceHandlers(ResourceHandlerRegistry registry) {
-	      registry.addResourceHandler("/resources/**").addResourceLocations("/webapp/resources/*");
-	  }
+
+	@Bean
+	public UrlBasedViewResolver setupViewResolver() {
+		UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+		resolver.setPrefix("/webapp/");
+		resolver.setSuffix(".jsp");
+		resolver.setViewClass(JstlView.class);
+		return resolver;
+	}
+
+	@Bean
+	public JpaTransactionManager jpaTransMan() {
+		JpaTransactionManager jtManager = new JpaTransactionManager(getEntityManagerFactoryBean().getObject());
+		return jtManager;
+	}
+
+	@Bean
+	public LocalEntityManagerFactoryBean getEntityManagerFactoryBean() {
+		LocalEntityManagerFactoryBean lemfb = new LocalEntityManagerFactoryBean();
+		lemfb.setPersistenceUnitName("localEntity");
+		return lemfb;
+	}
+
+	@Bean
+	public DataSource getDataSource() {
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/concretepage");
+		dataSource.setUsername("root");
+		dataSource.setPassword("");
+		return dataSource;
+	}
+
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/webapp/resources/*");
+	}
 }
